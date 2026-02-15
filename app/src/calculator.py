97bc8dc6e2a910dd9556aab8f0a57075ae2d60fa
Markus Winkler <winkler.mw@gmail.com>
2026-02-15 21:11:51 +0100

    feature: Add square root method to Calculator class

--- app/src/calculator.py
+++ app/src/calculator.py
@@ -1,4 +1,11 @@
+from src.sqrt_provider import SQRTProvider
+
 class Calculator:
+    sqrt_provider: SQRTProvider
+
+    def __init__(self, sqrt_provider: SQRTProvider) -> None:
+        self.sqrt_provider = sqrt_provider
+
     def add(self, a: float, b: float) -> float:
         if not isinstance(a, (int, float)) or not isinstance(b, (int, float)):
             raise TypeError("Both parameters must be of type float or int")
@@ -20,3 +27,14 @@ class Calculator:
         if b == 0:
             raise TypeError("Second parameter must not be zero")
         return float(a / b)
+    
+    def sqrt(self, a: float) -> float:
+        try:
+            value = float(a)
+        except ValueError:
+            raise Exception("Invalid number")
+
+        if value < 0:
+            raise Exception("Negative numbers are not allowed")
+
+        return self.sqrt_provider.run(a)
